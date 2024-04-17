d4157d6d1402a34043ec74b0eca74868b1453548
O4ku24 <ram.ne@mail.ru>
2024-04-17 13:46:47 +0700

    17.04

--- BANK/Bank.py
+++ BANK/Bank.py
@@ -1,7 +1,13 @@
+from typing import Any
 from Client import Client
+from Account import DebitAcc, CreditAcc
+from enum import Enum
 
 
 
+class AccountType(Enum):
+    CREDIT = 'credit'
+    DEBIT = 'debit'
 
 
 
@@ -15,6 +21,40 @@ class Bank:
         client = Client.fill()
         self.__client.append(client)
         print (f'Add client {self.add_client}')
+        return client
 
     def create_account(self):
         pass
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args,**kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+class BankManager(metaclass=Singleton):
+    def __init__(self) -> None:
+        super().__init__()
+        self.banks = []
+        self.client_pool = {}
+
+    def create_bank(self, name):
+        self.banks.append(name)
+
+    def create_client(self, client):
+        self.client_pool[client.get_phone()] = client
+
+
+
+
+
+
+
+
+
+
+
+        
--- BANK/main.py
+++ BANK/main.py
@@ -1,27 +1,32 @@
 
 from Client import Client
-from Bank import Bank
-import uvicorn
-from database import  engine
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from Bank import Bank, BankManager, AccountType
+from Account import DebitAcc, CreditAcc
 
 
 
-app = FastAPI(
-    title="TodoList",
-    version="0.0.1"
-)
-
-""" app.include_router(router)
-app.include_router(task_temp_url) """
 
 
-if __name__ == '__main__':
-    Client.metadata.create_all(engine)
-    Bank.metadata.create_all(engine)
-    print('Start Server')
-    uvicorn.run('main:app', port = 8000, host='127.0.0.1', reload=True)
-    print('Server Stop')
+
+
+
+
+
+
+bm = BankManager()
+bm.create_bank('VTB')
+print(bm.banks)
+
+user_one = Client.fill()
+
+print(user_one)
+
+
+
+
+
+
+
+
 
  
